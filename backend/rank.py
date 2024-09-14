fe7efbaf5049d35624d07e93345f970ffdaac18f
Charles Yuan <charlesyuan59@gmail.com>
2024-09-14 15:44:06 -0400

    Added ranking algorithm for matches

---
+++ backend/rank.py
@@ -0,0 +1,134 @@
+import googlemaps
+import json
+import os
+import pandas as pd
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+gmaps = googlemaps.Client(key=os.getenv("GOOGLE_MAPS_API_KEY"))
+
+
+def calculate_distance(address1, address2):
+    '''
+    Calculate the distance between two addresses in km.
+
+    Args:
+        address1: str
+        address2: str
+
+    Returns:
+        distance (km): str
+    '''
+    start_location = gmaps.geocode(address1)[0]['geometry']['location']
+    end_location = gmaps.geocode(address2)[0]['geometry']['location']
+
+    directions_result = gmaps.directions(
+        origin=(start_location['lat'], start_location['lng']),
+        destination=(end_location['lat'], end_location['lng']),
+        mode="driving"
+    )
+
+    route = directions_result[0]
+    legs = route['legs'][0]
+    distance = legs['distance']['text']
+
+    return distance
+
+
+def is_match(user1_data, user2_data):
+    '''
+    Args: 
+        user1_data: dict
+        user2_data: dict
+
+    Returns:
+        is_match: bool
+    '''
+    user1_start_location = user1_data["start_location"]
+    user1_end_location = user1_data["end_location"]
+    user1_departure_time = user1_data["departure_time"]
+
+    user2_start_location = user2_data["start_location"]
+    user2_end_location = user2_data["end_location"]
+    user2_departure_time = user2_data["departure_time"]
+    
+    # Check if user1 and user2 start and end within 3 km of each other
+    start_difference = calculate_distance(user1_start_location, user2_start_location)
+    end_difference = calculate_distance(user1_end_location, user2_end_location)
+    start_difference, end_difference = float(start_difference.split()[0]), float(end_difference.split()[0])
+
+    # Get difference between departure times in minutes
+    time_format = "%Y-%m-%dT%H:%M:%S"
+    user1_departure_datetime = datetime.strptime(user1_departure_time, time_format)
+    user2_departure_datetime = datetime.strptime(user2_departure_time, time_format)
+    diff_in_minutes = (user1_departure_datetime - user2_departure_datetime).total_seconds() / 60
+
+    if start_difference <= 3 and end_difference <= 3 and diff_in_minutes <= 10:
+        return True
+    return False
+
+
+def rank(user_data, matches_data):
+    '''
+    Rank based on the following:
+        1. Distance between the start and end locations of the two users (in km)
+        2. Difference in departure times (in minutes)
+        3. Vehicle type (electric > hybrid > gasoline)
+    
+    Args:
+        user_data: pd.Series
+        matches_data: pd.DataFrame
+
+    Returns:
+        df_sorted: pd.DataFrame
+    '''
+    # Create a copy of the matches_data to avoid SettingWithCopyWarning
+    df = matches_data.copy()
+
+    # Ensure matches are valid based on maximum distance diff and time diff
+    df = df[df.apply(lambda row: is_match(row, user_data), axis=1)]
+
+    # Convert departure_time to datetime for both user_data and df
+    user_departure_time = pd.to_datetime(user_data['departure_time'])
+    df['departure_time'] = pd.to_datetime(df['departure_time'])
+
+    # Calculate distance difference between the start and end locations
+    df['start_location_diff'] = df['start_location'].apply(lambda x: calculate_distance(user_data['start_location'], x))
+    df['end_location_diff'] = df['end_location'].apply(lambda x: calculate_distance(user_data['end_location'], x))
+    df['distance'] = df['start_location_diff'] + df['end_location_diff']
+
+    # Calculate the difference in departure times
+    df['time_diff'] = (df['departure_time'] - user_departure_time).dt.total_seconds() / 60
+
+    # Create a numeric column for vehicle type sorting
+    vehicle_type_order = {'electric': 2, 'hybrid': 1, 'gasoline': 0}
+    df['vehicle_type_numeric'] = df['vehicle_type'].map(vehicle_type_order)
+
+    # Sort the DataFrame and drop temp cols
+    df_sorted = df.sort_values(by=['distance', 'time_diff', 'vehicle_type_numeric'], 
+                               ascending=[True, True, False])
+    df_sorted = df_sorted.drop(['start_location_diff', 'end_location_diff', 'vehicle_type_numeric'], axis=1)
+
+    return df_sorted
+
+
+if __name__ == "__main__":
+    with open('backend/examples.json', 'r') as file:
+        data = json.load(file)
+
+    # Create DataFrame
+    df = pd.DataFrame(data)
+
+    # Ensure all columns except departure_time are strings
+    string_columns = ['username', 'vehicle_type', 'start_location', 'end_location']
+    for col in string_columns:
+        df[col] = df[col].astype(str)
+
+    # Create sample dataframe
+    user_data = df.iloc[0]
+    matches_data = df.iloc[1:]
+
+    # Rank the matches
+    df_sorted = rank(user_data, matches_data)
+    print(df_sorted)
